a37197cda4b7bd05a6aba8271a5b86d3591ef5fc
Krushna <krishnaydv35@gmail.com>
2024-03-03 13:15:32 +0530

    added files

---
+++ src/co/jsp/LMS/model/Library.java
@@ -0,0 +1,51 @@
+package co.jsp.LMS.model;
+
+import java.util.List;
+
+ 
+public class Library {
+	private String libraryName;
+	private String libraryAddress;
+	private int pincode;
+	
+	private List<Book> books;
+
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	public String getLibraryName() {
+		return libraryName;
+	}
+
+	public void setLibraryName(String libraryName) {
+		this.libraryName = libraryName;
+	}
+
+	public String getLibraryAddress() {
+		return libraryAddress;
+	}
+
+	public void setLibraryAddress(String libraryAddress) {
+		this.libraryAddress = libraryAddress;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+
+
+
+	
+
+}
